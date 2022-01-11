fbc0c2d3652aff833b056b6e6a1303174b6081fd
fradesigner <fradesigner@gmail.com>
2022-01-11 11:48:03 +0100

    first commit

---
+++ kibana/x-pack/plugins/upgrade_assistant/__jest__/client_integration/overview/migrate_system_indices/flyout.test.ts
@@ -0,0 +1,46 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { act } from 'react-dom/test-utils';
+
+import { OverviewTestBed, setupOverviewPage } from '../overview.helpers';
+import { setupEnvironment, kibanaVersion } from '../../helpers';
+import { systemIndicesMigrationStatus } from './mocks';
+
+if (kibanaVersion.major >= 8 || kibanaVersion.minor > 16) {
+  describe('Overview - Migrate system indices - Flyout', () => {
+    let testBed: OverviewTestBed;
+    const { server, httpRequestsMockHelpers } = setupEnvironment();
+
+    beforeEach(async () => {
+      httpRequestsMockHelpers.setLoadSystemIndicesMigrationStatus(systemIndicesMigrationStatus);
+
+      await act(async () => {
+        testBed = await setupOverviewPage();
+      });
+
+      testBed.component.update();
+    });
+
+    afterAll(() => {
+      server.restore();
+    });
+
+    test('shows correct features in flyout table', async () => {
+      const { actions, table } = testBed;
+
+      await actions.clickViewSystemIndicesState();
+
+      const { tableCellsValues } = table.getMetaData('flyoutDetails');
+
+      expect(tableCellsValues.length).toBe(systemIndicesMigrationStatus.features.length);
+      expect(tableCellsValues).toMatchSnapshot();
+    });
+  });
+} else {
+  test(`System indices migration is disabled for ${kibanaVersion.version}`, () => undefined);
+}
---
+++ kibana/x-pack/plugins/upgrade_assistant/public/application/components/overview/upgrade_step/upgrade_step.tsx
@@ -0,0 +1,38 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import React from 'react';
+
+import { EuiText, EuiSpacer } from '@elastic/eui';
+import { i18n } from '@kbn/i18n';
+import type { EuiStepProps } from '@elastic/eui/src/components/steps/step';
+
+const i18nTexts = {
+  upgradeStepTitle: i18n.translate('xpack.upgradeAssistant.overview.upgradeStepTitle', {
+    defaultMessage: `Check back here when it's time to upgrade to 8.x`,
+  }),
+  upgradeStepDescription: i18n.translate('xpack.upgradeAssistant.overview.upgradeStepDescription', {
+    defaultMessage: `If you've resolved all critical issues and checked your deprecation logs, you're ready for the next major version of Elastic. Check this page again before upgrading to Elastic 8.x.`,
+  }),
+};
+
+export const getUpgradeStep = (): EuiStepProps => {
+  return {
+    title: i18nTexts.upgradeStepTitle,
+    status: 'incomplete',
+    'data-test-subj': 'upgradeStep',
+    children: (
+      <>
+        <EuiText>
+          <p>{i18nTexts.upgradeStepDescription}</p>
+        </EuiText>
+
+        <EuiSpacer size="m" />
+      </>
+    ),
+  };
+};
